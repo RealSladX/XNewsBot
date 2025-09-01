8ef847ccc2077c95022a23193cc4c3ce2d3a2e9e
RealSladX <imsladx@gmail.com>
2025-09-01 12:13:13 -0700

    fixed database

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ def init_db():
             url TEXT NOT NULL UNIQUE,
             summary TEXT,
             score REAL,
-            img_url TEXT UNIQUE,
+            img_url TEXT,
             crawl_timestamp DATETIME NOT NULL
         )
     """)
